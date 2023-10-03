a10a469fdd31d67a913d4c51497cca1285ef72ea
Eduardo Bolson Rauber <eduardo_farm@hotmail.com>
2023-10-02 21:59:54 -0300

    Menu e program atualizados

--- Drop2.0/Menu.cs
+++ Drop2.0/Menu.cs
@@ -12,7 +12,7 @@ namespace Drop2._0
        public static string MenuPrincipal()
         {
             Console.WriteLine("------ MENU PRINCIPAL ------");
-            Console.WriteLine("0- Sair");
+            Console.WriteLine();
             Console.WriteLine("1- Cadastre-se aqui");
             Console.WriteLine("2- Lista de Produtos");
             Console.WriteLine("3- Carrinho");
@@ -20,6 +20,9 @@ namespace Drop2._0
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine("9- Área do vendedor");
+            Console.WriteLine("0- Sair");
+            Console.WriteLine();
+            Console.Write("Digite a opção desejada: ");
 
             return Console.ReadLine();
         }
@@ -27,7 +30,7 @@ namespace Drop2._0
         public static string MenuProdutos()
         {
             Console.WriteLine("------ SELEÇÃO DOS TIMES ------");
-            Console.WriteLine("0- Retornar ao menu anterior");
+            Console.WriteLine();
             Console.WriteLine("1- Vasco da Gama");
             Console.WriteLine("2- Flamengo");
             Console.WriteLine("3- Brasil");
@@ -39,6 +42,10 @@ namespace Drop2._0
             Console.WriteLine("9- Paris Saint-Germain");
             Console.WriteLine("10- Arsenal");
             Console.WriteLine("11- Barcelona ");
+            Console.WriteLine();
+            Console.WriteLine("0- Retornar ao menu anterior");
+            Console.WriteLine();
+            Console.Write("Digite a opção desejada: ");
 
             return Console.ReadLine();
         }
@@ -46,15 +53,17 @@ namespace Drop2._0
         public static string MenuVendedor()
         {
             Console.WriteLine("------ FERRAMENTAS DO VENDEDOR ------");
-            Console.WriteLine("0- Retornar ao menu anterior");
+            Console.WriteLine();
             Console.WriteLine("1- Cadastrar novo produto");
-            Console.WriteLine("2- Alterar produto");
-            Console.WriteLine("3- ");
+            Console.WriteLine("2- Lista de Produtos");
+            Console.WriteLine("3- Alterar produto");
+            Console.WriteLine("4- Remover produto");
+            Console.WriteLine();
+            Console.WriteLine("0- Retornar ao menu anterior");
+            Console.WriteLine();
+            Console.Write("Digite a opção desejada: ");
 
             return Console.ReadLine();
         }
-        
-        
-
     }
 }
