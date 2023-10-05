1795106efdab54c180f975f67871ad5fbdd945af
Djordhan <djordhandarosa@hotmail.com>
2023-10-04 21:06:00 -0300

    Criação de metodos no pagamento boleto e pix

--- Drop2.0/Pix.cs
+++ Drop2.0/Pix.cs
@@ -12,5 +12,37 @@ namespace Drop2._0
         public string ChaveEmail { get; set; }
         public int Cnpj { get; set; }
 
+
+
+
+
+        public void PagamentoPix()
+        {
+            Console.WriteLine("Escolha a chave pix que deseja pagar: 1: Chave aleatória, 2: E-Mail, 3: CNPJ");
+            int opcao = Convert.ToInt32(Console.ReadLine());
+
+            switch(opcao) {
+
+                case 1: 
+                        Console.WriteLine($"A sua compra ficou no valor de: R${ValorTotal:N2}");
+                        Console.WriteLine("Nossa chave aleatória é: 'q32b8y1s6w0z4v9t' ");
+
+                    break;
+
+                case 2: 
+                        Console.WriteLine($"A sua compra ficou no valor de: R${ValorTotal:N2}");
+                        Console.WriteLine("Nossa Chave do email é: 'Dropdosguri@Gmail.com' ");
+                    break;
+
+                case 3:
+                    Console.WriteLine($"A sua compra ficou no valor de: R${ValorTotal:N2}");
+                    Console.WriteLine("Nosso CNJP é: '12.345.678/0001-90' ");
+                    break;
+            }
+
+                 Console.WriteLine("O Drop dos guri agradece a preferência, volte sempre!! =) ");
+                Console.ReadLine();
+        }
     }
+
 }
